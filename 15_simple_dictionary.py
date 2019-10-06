8041ead21b5d3bbac7e51fde235ffb362faf7ecd
shubhamv1436 <shubhamv1436@gmail.com>
2019-10-07 00:06:44 +0530

    Added Q15-simple dictionary

---
+++ 15_simple_dictionary.py
@@ -0,0 +1,20 @@
+# Difficulty Level: Beginner
+
+# Question: Create a dictionary that contains the keys a  and b  and their
+# respective values 1  and 2 .
+
+# Hint: Dictionaries can be created either by using curly brackets or by using
+# the dict  function.
+
+# Program
+# Solution 1
+simple_dict = {'a':1, 'b':2}
+print(simple_dict)
+
+# Solution 2
+simple_dict = dict(a = 1, b = 2)
+print(simple_dict)
+
+# Output
+# shubhamvaishnav:python-bootcamp$ python3 15_simple_dictionary.py 
+# {'a': 1, 'b': 2}
