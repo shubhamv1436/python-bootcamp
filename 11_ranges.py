fc76b277acf944f27b83d596c18e70d40517ed0c
shubhamv1436 <shubhamv1436@gmail.com>
2019-10-07 00:06:08 +0530

    Added Q11-ranges

---
+++ 11_ranges.py
@@ -0,0 +1,13 @@
+# Difficulty Level: Beginner
+
+# Question: Create a list using range method
+
+# Expected output:
+# [1,2,3,4,5,6,7,8,9,10]
+
+# Program
+print([i for i in range(1,11)])
+
+# Output
+# shubhamvaishnav:python-bootcamp$ python3 11_ranges.py
+# [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
