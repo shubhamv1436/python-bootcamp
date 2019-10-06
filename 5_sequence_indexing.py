4c8aaabb2c663c2212cd001781c894f40270ed1c
shubhamv1436 <shubhamv1436@gmail.com>
2019-10-06 18:38:20 +0530

    Added Q5-sequence indexing

---
+++ 5_sequence_indexing.py
@@ -0,0 +1,21 @@
+# Difficulty Level: Beginner
+
+# Question: Complete the script so that it prints out the second item of the
+# list.
+
+# letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+# Expected output:
+# b
+
+# Hint: List indexing starts from 0.
+
+# Program
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+print(letters[1])
+
+# Output
+# shubhamvaishnav:python-bootcamp$ python3 5_sequence_indexing.py
+# b
+
+# Reason: "b" character is present at position 2 and indexing starts with 0
