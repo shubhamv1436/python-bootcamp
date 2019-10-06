401c3fe862d4738b6664e0d806566f796f1e3c94
shubhamv1436 <shubhamv1436@gmail.com>
2019-10-07 00:06:52 +0530

    Added Q16-accessing dictionary items

---
+++ 16_accessing_dictionary_items.py
@@ -0,0 +1,15 @@
+# Difficulty Level: Beginner
+
+# Question: Please complete the script so that it prints out the value of key b.
+# d = {"a": 1, "b": 2}
+
+# Expected output:
+# 2
+
+# Program
+d = {"a": 1, "b": 2}
+print(d["b"])
+
+# Output
+# shubhamvaishnav:python-bootcamp$ python3 16_accessing_dictionary_items.py 
+# 2
