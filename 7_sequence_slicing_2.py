276ae4547247e6625089f695581f3d6995b3fdf4
shubhamv1436 <shubhamv1436@gmail.com>
2019-10-06 18:39:33 +0530

    Added Q7-sequence slicing 2

---
+++ 7_sequence_slicing_2.py
@@ -0,0 +1,23 @@
+# Difficulty Level: Beginner
+
+# Question: List slicing is important in various data manipulation activities.
+# Let's do a few more exercises on that.
+
+# Please complete the script so that it prints out the first three items of list
+# letters.
+
+# letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+# Expected output:
+# ['a', 'b', 'c']
+
+# Hint: You can do that with letters[0:3] , but there's even a shorter way to
+# do that.
+
+# Program
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+print(letters[:3])
+
+# Output
+# shubhamvaishnav:python-bootcamp$ python3 7_sequence_slicing_2.py
+# ['a', 'b', 'c']
