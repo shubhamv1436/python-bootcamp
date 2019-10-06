6f5f77a37027b2a04d70937738b7b0137b86fc58
shubhamv1436 <shubhamv1436@gmail.com>
2019-10-06 17:57:54 +0530

    Added Q1-variable updating

---
+++ 1_variable_updating.py
@@ -0,0 +1,18 @@
+# Difficulty Level: Beginner
+
+# Question: Let's start with easy things first. What will the following code produce?
+
+# a = 2
+# a = 4
+# a = 6
+# print(a + a + a)
+
+# Program
+a = 2
+a = 4
+a = 6
+print(a + a + a)
+
+# Output
+# shubhamvaishnav:python-bootcamp$ python3 1_variable_updating.py
+# 18
