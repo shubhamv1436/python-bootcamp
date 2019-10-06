5f03f605ba65db0f9a0298f4b3d85e9f2d04692e
shubhamv1436 <shubhamv1436@gmail.com>
2019-10-07 00:07:00 +0530

    Added Q17-dictionary items sum up

---
+++ 17_dictionary_items_sum_up.py
@@ -0,0 +1,15 @@
+# Difficulty Level: Beginner
+
+# Question: Calculate the sum of the values of keys a  and b .
+# d = {"a": 1, "b": 2, "c": 3}
+
+# Expected output:
+# 3
+
+# Program
+d = {"a": 1, "b": 2, "c": 3}
+print(d["a"] + d["b"])
+
+# Output
+# shubhamvaishnav:python-bootcamp$ python3 17_dictionary_items_sum_up.py
+# 3
