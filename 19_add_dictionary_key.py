5247aceb505fc80fbe507b74eee784d89bc5da83
shubhamv1436 <shubhamv1436@gmail.com>
2019-10-07 00:07:17 +0530

    Added Q19-add dictionary key

---
+++ 19_add_dictionary_key.py
@@ -0,0 +1,17 @@
+# Difficulty Level: Easy
+
+# Question: Add a new pair of key (e.g. c ) and value (e.g. 3 ) to the
+# dictionary and print out the new dictionary.
+# d = {"a": 1, "b": 2}
+
+# Expected output:
+#   {'a': 1, 'c': 3, 'b': 2}
+
+# Program
+d = {"a": 1, "b": 2}
+d["c"] = 3
+print(d)
+
+# Output
+# shubhamvaishnav:python-bootcamp$ python3 19_add_dictionary_key.py
+# {'a': 1, 'b': 2, 'c': 3}
