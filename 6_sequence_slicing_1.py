bd238559b296b0fe41c4f175a0648cb40da3e6a4
shubhamv1436 <shubhamv1436@gmail.com>
2019-10-06 18:39:19 +0530

    Added Q6-sequence slicing 1

---
+++ 6_sequence_slicing_1.py
@@ -0,0 +1,18 @@
+# Difficulty Level: Beginner
+
+# Question: Please complete the script so that it prints out a list slice
+# containing items d , e , and f .
+# letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+# Expected output:
+# ['d', 'e', 'f']
+
+# Hint: Keep in mind that list slicing is upper-bound exclusive.
+
+# Program
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+print(letters[3:6])
+
+# Output
+# shubhamvaishnav:python-bootcamp$ python3 6_sequence_slicing.py
+# ['d', 'e', 'f']
