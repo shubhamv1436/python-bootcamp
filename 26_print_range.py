d98d729b8efe017ad2fa43f416c99b2b744f7d62
shubhamv1436 <shubhamv1436@gmail.com>
2019-10-07 22:46:23 +0530

    Added Q26-print range

---
+++ 26_print_range.py
@@ -0,0 +1,33 @@
+# Difficulty Level: Beginner
+
+# Question: Make a script that prints out numbers from 1 to 10
+
+# Expected output:
+#
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+
+# Program
+for i in range(1,11):
+    print(i)
+
+# Output
+# shubhamvaishnav:python-bootcamp$ python3 26_print_range.py
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
